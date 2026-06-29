5f6a9c4a716aa7ed06cdac7403e8736c7865b157
Ildar <bogdanovildar31@gmail.com>
2026-06-29 11:39:50 +0600

    Первый коммит
    git add index.html#

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import View from "./components/ViewUsers/View"
+
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <View />
+    </div>
+  )
+}
+
+
+export default App
